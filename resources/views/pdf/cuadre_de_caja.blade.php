8becaf0cafe51a6ab61b5c46b3a6b733108e8c05
Hector Damas <hectorgabrieldm@hotmail.com>
2025-05-29 10:35:05 -0400

    margen cuadre

--- resources/views/pdf/cuadre_de_caja.blade.php
+++ resources/views/pdf/cuadre_de_caja.blade.php
@@ -10,7 +10,7 @@
         body {
             font-family: DejaVu Sans, sans-serif;
             font-size: 11px;
-            margin: 1cm;
+            margin: 0 1cm;
             color: #333;
         }
 
