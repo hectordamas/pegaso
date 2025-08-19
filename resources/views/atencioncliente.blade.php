be17ad35bc50260150de7f7d4a128d436343f630
Hector Damas <hectorgabrieldm@hotmail.com>
2025-08-19 15:18:25 -0400

    fecha atencion cliente

--- resources/views/atencioncliente.blade.php
+++ resources/views/atencioncliente.blade.php
@@ -100,7 +100,7 @@
                             @foreach ($atencionClientes as $item)
                             <tr>
                                 <td>{{ $item->id }}</td>
-                                <td><p class="fecha">{{ \Carbon\Carbon::parse($item->fecha)->format('d-m-Y g:i A') }}</p></td>
+                                <td><p class="fecha">{{ $item->created_at->format('d-m-Y g:i A') }}</p></td>
                                 <td><p>{{ $item->saclie->descrip ?? 'N/A' }}</p></td>
                                 <td>
                                     <span class="badge" style="background: {{$item->estatusAt->color}};">
