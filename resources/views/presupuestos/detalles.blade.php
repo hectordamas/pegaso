fed6b1e359b1774c0ee26014bf522d5c5ddb20d3
Hector Damas <hectorgabrieldm@hotmail.com>
2025-12-02 17:07:01 -0400

    soporte

--- resources/views/presupuestos/detalles.blade.php
+++ resources/views/presupuestos/detalles.blade.php
@@ -11,27 +11,34 @@
                         <td><small>Precio</small></td>
                         <td><small>Cant.</small></td>
                         <td><small>Total Item</small></td>
+                        <td><small>Total Item ($)</small></td>
+                        <td><small>Factor</small></td>
                         <td><small>IVA</small></td>
                         <td><small>Total</small></td>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($items as $index => $item)
-                    <tr>
-                        <td>{{$index + 1}}</td>
-                        <td>{{$item->CodItem}}</td>
-                        <td>{{$item->CodUbic}}</td>
-                        <td>
-                            <small>
-                                {!! implode('<br>', array_filter([$item->Descrip1, $item->Descrip2, $item->Descrip3, $item->Descrip4, $item->Descrip5])) !!}
-                            </small>
-                        </td>            
-                        <td>{{ number_format($item->Precio, 2, '.', ',') }}</td>
-                        <td>{{ $item->Cantidad }}</td>
-                        <td>{{ number_format($item->TotalItem, 2, '.', ',') }}</td>
-                        <td>{{ number_format($item->MtoTax, 2, '.', ',') }}</td>
-                        <td>{{ number_format($item->TotalItem + $item->MtoTax, 2, '.', ',') }}</td>
-                    </tr>
+                    @foreach ($items as $index => $item)
+                        <tr>
+                            <td>{{ $index + 1 }}</td>
+                            <td>{{ $item->CodItem }}</td>
+                            <td>{{ $item->CodUbic }}</td>
+                            <td>
+                                <small>
+                                    {!! implode(
+                                        '<br>',
+                                        array_filter([$item->Descrip1, $item->Descrip2, $item->Descrip3, $item->Descrip4, $item->Descrip5]),
+                                    ) !!}
+                                </small>
+                            </td>
+                            <td>{{ number_format($item->Precio, 2, '.', ',') }}</td>
+                            <td>{{ $item->Cantidad }}</td>
+                            <td>{{ number_format($item->TotalItem, 2, '.', ',') }}</td>
+                            <td>{{ number_format($item->TotalItem / $item->factor ?: 1, 2, '.', ',') }}</td>
+                            <td>{{ number_format($item->factor ?: 1, 2, '.', ',') }}</td>
+                            <td>{{ number_format($item->MtoTax, 2, '.', ',') }}</td>
+                            <td>{{ number_format($item->TotalItem + $item->MtoTax, 2, '.', ',') }}</td>
+                        </tr>
                     @endforeach
                 </tbody>
                 <tfoot class="table-dark">
@@ -42,6 +49,16 @@
                         <td>{{ number_format($items->sum('Precio'), 2, '.', ',') }}</td>
                         <td>{{ number_format($items->sum('Cantidad'), 2, '.', ',') }}</td>
                         <td>{{ number_format($items->sum('TotalItem'), 2, '.', ',') }}</td>
+                        <td>{{ number_format(
+                            $items->sum(function ($item) {
+                                return $item->TotalItem / ($item->factor ?: 1);
+                            }),
+                            2,
+                            '.',
+                            ',',
+                        ) }}
+                        </td>
+                        <td></td>
                         <td>{{ number_format($items->sum('MtoTax'), 2, '.', ',') }}</td>
                         <td>{{ number_format($items->sum('TotalItem') + $items->sum('MtoTax'), 2, '.', ',') }}</td>
                     </tr>
@@ -52,10 +69,10 @@
 </div>
 
 
-@if($presupuesto->razon)
-<div class="row">
-    <div class="col-md-12 mt-3">
-        <p><strong>Razón: </strong> {{ $presupuesto->razon }}</p>
+@if ($presupuesto->razon)
+    <div class="row">
+        <div class="col-md-12 mt-3">
+            <p><strong>Razón: </strong> {{ $presupuesto->razon }}</p>
+        </div>
     </div>
-</div>
 @endif
